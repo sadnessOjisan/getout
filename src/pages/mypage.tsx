aa599d559f127bd52dcbdf5d84596a955317e1df
sadness_ojisan <sadness.ojisan@gmail.com>
2022-01-30 14:00:29 +0900

    Add mypage

---
+++ src/pages/mypage.tsx
@@ -0,0 +1,45 @@
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+import { useEffect, VFC } from "react";
+
+type Props =
+  | {
+      _tag: "s";
+      data: string;
+    }
+  | {
+      _tag: "f";
+      message: string;
+    };
+
+const Mypage: VFC<Props> = (props) => {
+  return (
+    <div>
+      {props._tag === "s" ? (
+        <div>my name is {props.data}</div>
+      ) : (
+        <div>{props.message}</div>
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
+  const cookie = ctx.req.cookies["auth"];
+  if (!cookie) {
+    return {
+      props: {
+        _tag: "f",
+        message: "ログインしてください",
+      },
+    };
+  }
+  return {
+    props: {
+      _tag: "s",
+      data: "d",
+    },
+  };
+};
+
+export default Mypage;
